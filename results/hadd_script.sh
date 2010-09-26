83c45973fe6a76d6b684f229c3d246424b626c2d
Bastian Beischer <bastian.beischer@gmail.com>
2010-09-26 21:12:30 +0200

    changed hadd script

--- results/hadd_script.sh
+++ results/hadd_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(find . -regex ".*_electrons_consistency_000\.root$" | sort); do
+for i in $(find . -regex ".*_electrons_000\.root$" | sort); do
     stem=$(echo $i | sed 's/\(.*\)\(_000\)\(\.root\)/\1/');
-    hadd -f ${stem}.root ${stem}_00[0-9].root;
+    hadd -f ${stem}.root ${stem}_0[01][0-9].root;
 done;
