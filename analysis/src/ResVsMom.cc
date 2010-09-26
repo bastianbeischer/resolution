41d296167202abaca58ede8639b42f63876ef054
Bastian Beischer <bastian.beischer@gmail.com>
2010-09-26 18:01:57 +0200

    labels, legend, stats...

--- analysis/src/ResVsMom.cc
+++ analysis/src/ResVsMom.cc
@@ -2,6 +2,9 @@
 
 #include <TCanvas.h>
 #include <TGraphErrors.h>
+#include <TPaveStats.h>
+#include <TLegend.h>
+#include <TLatex.h>
 #include <TTree.h>
 #include <TFile.h>
 #include <TH1D.h>
@@ -15,12 +18,24 @@
 int ResVsMom::m_markers[4] = {23, 22, 29, 27};
 int ResVsMom::m_colors[4] = {kRed, kBlue, kBlack, kViolet};
 
+double fitfunc(double* x, double* p)
+{
+  //  double m = 5.11e-4;
+  double m = p[2];
+  double beta = x[0] / sqrt(pow(x[0],2.) + pow(m,2.));
+  return sqrt(pow(x[0]*p[0],2.) + pow(p[1]/beta, 2.));
+}
+
 ResVsMom::ResVsMom()
 {
 }
 
 ResVsMom::~ResVsMom()
 {
+  for (unsigned int i = 0; i < m_graphs.size(); i++) {
+    delete m_graphs.at(i);
+  }
+  m_graphs.clear();
 }
 
 void ResVsMom::addGraph(const char* fileNameTemplate, double min, double max, double step, double guessA = 0.08, double guessB = 0.21)
@@ -74,11 +89,34 @@ void ResVsMom::addGraph(const char* fileNameTemplate, double min, double max, do
     file.Close();
   }
 
+  delete genEvent;
+  delete recEvent;
+
   graph->SetMarkerStyle(m_markers[m_graphs.size()%4]);
   graph->SetMarkerColor(m_colors[m_graphs.size()%4]);
   graph->SetTitle("Momentum resolution for PERDaix at 1^{o} stereo angle");
 
   m_graphs.push_back(graph);
+
+  // fit
+  double m_electron = 5.11e-4;
+  //double m_proton   = 9.38e-1;
+
+  TF1 fitMSC("fitMSC", fitfunc, min, max, 3);
+  fitMSC.SetParNames("a", "b");
+  fitMSC.SetParNames("a", "b", "m");
+  TF1 fitNoMSC("fitNoMSC", "[0]*x + [1]", min, max);
+  fitNoMSC.SetParNames("a", "b");
+
+  fitMSC.FixParameter(2, m_electron);
+  graph->Fit("fitMSC", "E");
+  graph->SetMarkerSize(1.5);
+  graph->GetFunction("fitMSC")->SetLineColor(graph->GetMarkerColor());
+  
+  graph->GetXaxis()->SetTitle("p / GeV");
+  graph->GetYaxis()->SetTitle("#sigma_{p} / p");
+  graph->GetYaxis()->SetRangeUser(0.0, 1.2);
+
 }
 
 void ResVsMom::processConfigFile()
@@ -86,11 +124,11 @@ void ResVsMom::processConfigFile()
   double momMin = 0.25;
   double momMax = 10.0;
   double momStep = 0.25;
-  addGraph("../results/test.root", 0, 10, 1, 0.077, 0.18);
-  //  addGraph("../results/test.root", 0, 10, 1, 0.077, 0.18);
+  addGraph("../results/test.root", momMin, momMax, momStep, 0.077, 0.18);
+  // addGraph("../results/test.root", 0, 10, 1, 0.077, 0.18);
   // addGraph("../results/perdaix_%.2f_GeV_1.00_deg_msc_inhom_electrons_windows.root", momMin, momMax, momStep, 0.077, 0.18);
   // addGraph("../results/perdaix_%.2f_GeV_1.00_deg_msc_inhom_protons_windows.root", momMin, momMax, momStep, 0.077, 0.23);
-  //  addGraph("../results/perdaix_%.2f_GeV_1.00_deg_msc_inhom_protons_nowindows.root", momMin, momMax, momStep, 0.08, 0.30);
+  // addGraph("../results/perdaix_%.2f_GeV_1.00_deg_msc_inhom_protons_nowindows.root", momMin, momMax, momStep, 0.08, 0.30);
   // addGraph("../results/perdaix_%.2f_GeV_1.00_deg_nomsc_hom.root", momMin, momMax, momStep, 0.08, 0.0);
 
   draw();
@@ -104,81 +142,31 @@ void ResVsMom::draw()
   canvas->SetGridx();
   canvas->SetGridy();
 
-  for (int i = 0; i < m_graphs.size(); i++) {
+  for (unsigned int i = 0; i < m_graphs.size(); i++) {
     if (i == 0) m_graphs.at(i)->Draw("AP");
     else        m_graphs.at(i)->Draw("P");
+    m_graphs.at(i)->PaintStats(m_graphs.at(i)->GetFunction("fitMSC"));
   }
 
-  // double m_electron = 5.11e-4;
-  // double m_proton   = 9.38e-1;
+  TPaveStats* pt[m_graphs.size()];
+  TLatex text(3.1, 0.13, "#sigma_{p} / p = #sqrt{(ap)^{2} + (b/#beta)^{2}}");
+  TLegend legend(0.25, 0.75, 0.45, 0.88);
 
-  // TF1 fitMSC("fitMSC", fitfunc, momMin, momMax, 3);
-  // fitMSC.SetParNames("a", "b");
-  // fitMSC.SetParNames("a", "b", "m");
-  // TF1 fitNoMSC("fitNoMSC", "[0]*x + [1]", momMin, momMax);
-  // fitNoMSC.SetParNames("a", "b");
+  for (unsigned int i = 0; i < m_graphs.size(); i++) {
+    TPaveStats* stats = (TPaveStats*) m_graphs.at(i)->GetListOfFunctions()->FindObject("stats");
+    stats->SetTextColor(m_graphs.at(i)->GetMarkerColor());
+    pt[i] = stats;
+    pt[i]->SetX1NDC(0.7);
+    pt[i]->SetX2NDC(0.95);
+    pt[i]->SetY1NDC(0.38 - i*0.12);
+    pt[i]->SetY2NDC(0.48 - i*0.12);
 
-  // graph1.Draw("AP");
+    legend.AddEntry(m_graphs.at(i), "electrons", "P");
+    legend.AddEntry(m_graphs.at(i), "protons", "P");
+  }
 
-  // //  fitMSC.FixParameter(2, .938);
-
-  // fitMSC.FixParameter(2, m_electron);
-  // graph1.Fit("fitMSC", "E");
-  // graph1.SetMarkerSize(1.5);
-  // graph1.GetFunction("fitMSC")->SetLineColor(kRed);
-  // graph1.PaintStats(graph1.GetFunction("fitMSC"));
-  // graph2.Draw("P");
-  // fitMSC.FixParameter(2, m_proton);
-  // graph2.Fit("fitMSC", "E");
-  // graph2.SetMarkerSize(1.5);
-  // graph2.GetFunction("fitMSC")->SetLineColor(kBlue);
-  // graph2.PaintStats(graph2.GetFunction("fitMSC"));
-  // // graph3.Draw("P");
-  // // graph3.Fit("fitMSC", "E");
-  // // graph3.SetMarkerSize(1.5);
-  // // graph3.GetFunction("fitMSC")->SetLineColor(kBlack);
-  // // graph3.PaintStats(graph3.GetFunction("fitMSC"));
-  // // graph4.Draw("P");
-  // // graph4.Fit("fitNoMSC", "E");
-  // // graph4.SetMarkerSize(1.5);
-  // // graph4.GetFunction("fitNoMSC")->SetLineColor(kViolet);
-  // // graph4.PaintStats(graph4.GetFunction("fitNoMSC"));
-
-  // graph1.GetXaxis()->SetTitle("p / GeV");
-  // graph1.GetYaxis()->SetTitle("#sigma_{p} / p");
-  // graph1.GetYaxis()->SetRangeUser(0.0, 1.2);
-  // double upperRange = 10.0;
-  // graph1.GetXaxis()->SetLimits(0., upperRange);
-  // graph1.GetFunction("fitMSC")->SetRange(0., upperRange);
-  // graph2.GetFunction("fitMSC")->SetRange(0., upperRange);
-  // //  graph3.GetFunction("fitMSC")->SetRange(0., upperRange);
-  // // graph4.GetFunction("fitNoMSC")->SetRange(0., upperRange);
-
-  // TPaveStats* pt1 = (TPaveStats*) graph1.GetListOfFunctions()->FindObject("stats");
-  // pt1->SetTextColor(kRed);
-  // TPaveStats* pt2 = (TPaveStats*) graph2.GetListOfFunctions()->FindObject("stats");
-  // pt2->SetTextColor(kBlue);
-  // // TPaveStats* pt3 = (TPaveStats*) graph3.GetListOfFunctions()->FindObject("stats");
-  // // pt3->SetTextColor(kBlack);
-  // // TPaveStats* pt4 = (TPaveStats*) graph4.GetListOfFunctions()->FindObject("stats");
-  // // pt4->SetTextColor(kViolet);
-  // TPaveStats* pt[2] = {pt1, pt2};//, pt3, pt4};
-  // for (int i = 0; i < 2; i++) {
-  //   pt[i]->SetX1NDC(0.7);
-  //   pt[i]->SetX2NDC(0.95);
-  //   pt[i]->SetY1NDC(0.38 - i*0.12);
-  //   pt[i]->SetY2NDC(0.48 - i*0.12);
-  // }
-
-  // TLatex text(3.1, 0.13, "#sigma_{p} / p = #sqrt{(ap)^{2} + (b/#beta)^{2}}");
-  // text.Draw("SAME");
-
-  //  TLegend legend(0.25, 0.75, 0.45, 0.88);
-  //  legend.AddEntry(&graph1, "electrons", "P");
-  //  legend.AddEntry(&graph2, "protons", "P");
-  // // legend.AddEntry(&graph3, "module without windows", "P");
-  // // legend.AddEntry(&graph4, "Multiple Scattering deactivated + Hom. approximation in center", "P");
-  //  legend.Draw("SAME");
+  text.Draw("SAME");
+  legend.Draw("SAME");
 
   // canvas.SaveAs("perdaix_1_deg.pdf");
   // canvas.SaveAs("perdaix_1_deg.root");
