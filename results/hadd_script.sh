57838a74785cb7ea4f957406b710a103793a2ef4
Bastian Beischer <bastian.beischer@gmail.com>
2010-09-27 10:40:47 +0200

    delete files after hadding

--- results/hadd_script.sh
+++ results/hadd_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in $(find . -regex ".*_electrons_000\.root$" | sort); do
+for i in $(find . -regex ".*_protons_000\.root$" | sort); do
     stem=$(echo $i | sed 's/\(.*\)\(_000\)\(\.root\)/\1/');
-    hadd -f ${stem}.root ${stem}_0[01][0-9].root;
+    files=${stem}_0[01][0-9].root;
+    hadd -f ${stem}.root ${files};
+    rm -f ${files};
 done;
